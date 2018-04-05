6ef186a1805ed504c1bcd5efac5bdf3bf34b2c54
Pawel Wasil <wasil.paul@gmail.com>
2018-04-05 15:18:02 +0200

    add cw_4

---
+++ cw_4.js
@@ -0,0 +1,13 @@
+alert('waht the f ....??')
+
+var str = typeof("string");
+var x = typeof(1);
+var n = typeof(null);
+var bol = typeof(true);
+var obj = typeof([]);
+
+alert([str, x, n, bol, obj]);
+
+console.log('str', str, typeof str);
+console.log('x', x, typeof x);
+console.log('n', n, typeof n)
