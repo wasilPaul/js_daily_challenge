e175ee23f73a5f370c4078bacf8eaea7974da83e
Pawel Wasil <wasil.paul@gmail.com>
2018-11-04 22:45:21 +0100

    reduce method

--- JS/reduce.js
+++ JS/reduce.js
@@ -22,7 +22,7 @@ const averageMethod2 = euros.reduce((total, amount, index, array) => {
 
 console.log(averageMethod2); // 39.37
 
-/* like map */
+/* just like map method*/
 
 const doubled = euros.reduce((total, amount) => {
   total.push(amount * 2);
@@ -31,4 +31,82 @@ const doubled = euros.reduce((total, amount) => {
 
 console.log(doubled); // [ 59.52, 83.7, 93 ]
 
+/*just like filter method */
 
+const above40 = euros.reduce((total, amount) => {
+  if (amount > 40) {
+    total.push(amount);
+  }
+  return total;
+}, []);
+
+console.log(above40); // [ 41.85, 46.5 ]
+
+/*creating a tally */
+
+const fruitBasket = [
+  "banana",
+  "cherry",
+  "orange",
+  "apple",
+  "cherry",
+  "orange",
+  "apple",
+  "banana",
+  "cherry",
+  "orange",
+  "apple"
+];
+
+const counts = fruitBasket.reduce((tally, fruit) => {
+  tally[fruit] = (tally[fruit] || 0) + 1;
+  return tally;
+}, {});
+
+console.log(counts); // { banana: 2, cherry: 3, orange: 3, apple: 3 }
+
+const all = fruitBasket.reduce((tally, fruit) => {
+  tally[fruit] = 1;
+  return tally;
+}, {});
+
+console.log(all); // { banana: 1, cherry: 1, orange: 1, apple: 1 }
+
+const counts2 = fruitBasket.reduce((tally, fruit) => {
+  if (!tally[fruit]) {
+    tally[fruit] = 1;
+  } else {
+    tally[fruit] = tally[fruit] + 1;
+  }
+  return tally;
+}, {});
+
+console.log(counts2); // { banana: 2, cherry: 3, orange: 3, apple: 3 }
+
+/*flattening an array of arrays */
+
+const data = [
+  { a: "happy", b: "robin", c: ["blue", "green"] },
+  { a: "tired", b: "panther", c: ["green", "black", "orange", "blue"] },
+  { a: "sad", b: "goldfish", c: ["green", "red"] }
+];
+
+const colors = data.reduce((total, amount) => {
+  amount.c.forEach(color => {
+    total.push(color);
+  });
+  return total;
+}, []);
+
+console.log(colors); // [ 'blue','green', 'green', 'black', 'orange', 'blue', 'green', 'red' ]
+
+const uniqueColors = data.reduce((total, amount) => {
+  amount.c.forEach(color => {
+    if (total.indexOf(color) === -1) {
+      total.push(color);
+    }
+  });
+  return total;
+}, []);
+
+console.log(uniqueColors); // [ 'blue', 'green', 'black', 'orange', 'red' ]
